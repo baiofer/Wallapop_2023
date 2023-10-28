c9c4bc2eb0d39c953a451311a861afcef5e8e92d
Fernando Jarilla <fjarilla@gmail.com>
2023-10-28 09:06:06 +0200

    Product show in responsive grid

--- productList/productListView.js
+++ productList/productListView.js
@@ -2,10 +2,13 @@ export const buildProduct = (product) => {
     const newDiv = document.createElement('div')
     newDiv.classList.add('product')
     newDiv.innerHTML = `
-        <h3>${product.name}</h3>
-        <h4>Precio: ${product.price}€</h4>
-        <p>Tipo: ${product.sale ? 'venta' : 'compra'}</p>
-        <p>Categoria: ${product.tags[0]}</p>
+        <img src="${product.image ? product.image : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Imagen_no_disponible.svg/1024px-Imagen_no_disponible.svg.png"}" atl="Imagen del producto"/>
+        <div>
+            <h3>${product.name}</h3>
+            <h4>Precio: ${product.price}€</h4>
+            <p>Tipo: ${product.sale ? 'venta' : 'compra'}</p>
+            <p>Categoria: ${product.tags[0]}</p>
+        </div>
     ` 
     return newDiv 
 }
