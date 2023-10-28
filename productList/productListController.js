4f8068b5d309b79d405dea29aab36686384b5286
Fernando Jarilla <fjarilla@gmail.com>
2023-10-28 09:38:44 +0200

    Get products from sparrest

--- productList/productListController.js
+++ productList/productListController.js
@@ -1,9 +1,70 @@
 import { buildProduct } from "./productListView.js"
-import { products } from "./productListModel.js"
+import { getProducts } from "./productListModel.js"
 
-export const productListController = (productList) => {
+export const productListController = async (productList) => {
+    const products = await getProducts()
+    renderProducts(products, productList)
+}
+
+const renderProducts = (products, productList) => {
     products.forEach( product => {
         const newProduct = buildProduct(product)
         productList.appendChild(newProduct)
     })
 }
+
+/*
+import { buildTweet, emptyTweets } from './tweetListView.js'
+import { getTweets } from './tweetListModel.js'
+import { dispatchEvent } from "../utils/dispatchEvent.js"
+
+export const tweetListController = async (tweetList) => {
+    tweetList.innerHTML = ''
+    let tweets = []
+    try {
+        dispatchEvent('startLoadingTweets', null, tweetList)
+        tweets = await getTweets()
+    } catch (error) {
+        dispatchEvent('tweetsLoaded', {
+            type: 'error',
+            message: 'Error al cargar los tweets'
+        }, tweetList)
+    } finally {
+        dispatchEvent('finishLoadingTweets', null, tweetList)
+    }
+    
+    // 2.1 Crear estructura de html con los datos del tweet
+    
+    if (tweets.length === 0) {
+        tweetList.innerHTML = emptyTweets()
+    } else {
+        
+        renderTweets(tweets, tweetList)
+        dispatchEvent('tweetsLoaded', {
+            type: 'success',
+            message: 'Tweets cargados correctamente'
+        }, tweetList)
+    }
+    
+}
+
+const renderTweets = (tweets, tweetList) => {
+    tweets.forEach( tweet => {
+        const newTweet = document.createElement('div')
+        newTweet.classList.add('tweet')
+        newTweet.innerHTML = buildTweet(tweet)
+        // 2.2 Añado tweet al DOM
+        tweetList.appendChild(newTweet)
+    })
+}
+
+const createCustomEvent = (type, message) => {
+    const event = new CustomEvent('tweetsLoaded', {
+        detail: {
+            type: type,
+            message: message
+        }
+    })
+    return event
+}
+*/
--- productList/productListModel.js
+++ productList/productListModel.js
@@ -1,57 +1,6 @@
-export const products = [
-    { 
-        name: "Máquina de escribir", 
-        sale: true, 
-        price: 125, 
-        image: "https://cdn.pixabay.com/photo/2016/04/09/05/01/poet-1317444_1280.jpg", 
-        tags: ["work"] 
-    },
-    { 
-        name: "Máquina de fotos vintage", 
-        sale: true, 
-        price: 95, 
-        image: "https://cdn.pixabay.com/photo/2019/03/19/17/33/camera-4066340_1280.jpg", 
-        tags: ["work"] 
-    },
-    { 
-        name: "Medidor de tensión portátil", 
-        sale: true, 
-        price: 150, 
-        image: "https://cdn.pixabay.com/photo/2018/06/11/01/09/blood-pressure-monitor-3467664_1280.jpg", 
-        tags: ["lifestyle"] 
-    },
-    { 
-        name: "Motocicleta", 
-        sale: true, 
-        price: 3500,
-        image: "https://cdn.pixabay.com/photo/2019/02/27/22/43/scooter-4025114_1280.jpg", 
-        tags: ["motor"] 
-    },
-    { 
-        name: "Reloj de cocina", 
-        sale: true, 
-        price: 30, 
-        image: "https://cdn.pixabay.com/photo/2017/06/19/14/17/tray-2419460_1280.jpg", 
-        tags: ["lifestyle"]
-    },
-    { 
-        name: "Silla", 
-        sale: true, 
-        price: 275, 
-        image: "https://cdn.pixabay.com/photo/2017/08/06/01/49/table-2587598_1280.jpg", 
-        tags: ["lifestyle"] 
-    },
-    { 
-        name: "iPhone 12", 
-        sale: false, 
-        price: 550, 
-        image: "https://cdn.pixabay.com/photo/2021/01/03/02/24/iphone-12-pro-5883473_1280.jpg",
-        tags: ["mobile"] 
-    },
-    { 
-        name: "iWatch", 
-        sale: false, 
-        price: 225, 
-        tags: ["mobile"] 
-    }
-]
+export const getProducts = async () => {
+    const url = 'http://localhost:8000/api/products'
+    const response = await fetch(url)
+    const products = await response.json()
+    return products
+}
