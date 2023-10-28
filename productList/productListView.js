4446e28a449bf282fc10c557cd21645169b408a6
Fernando Jarilla <fjarilla@gmail.com>
2023-10-28 20:00:56 +0200

    Added navigation to productDetail

--- productList/productListView.js
+++ productList/productListView.js
@@ -2,13 +2,15 @@ export const buildProduct = (product) => {
     const newDiv = document.createElement('div')
     newDiv.classList.add('product')
     newDiv.innerHTML = `
-        <img src="${product.image ? product.image : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Imagen_no_disponible.svg/1024px-Imagen_no_disponible.svg.png"}" atl="Imagen del producto"/>
-        <div>
-            <h3>${product.name}</h3>
-            <h4>Precio: ${product.price}€</h4>
-            <p>Tipo: ${product.sale ? 'venta' : 'compra'}</p>
-            <p>Categoria: ${product.tags[0]}</p>
-        </div>
+        <a href="../productDetail.html?id=${product.id}">
+            <img src="${product.image ? product.image : "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/Imagen_no_disponible.svg/1024px-Imagen_no_disponible.svg.png"}" atl="Imagen del producto"/>
+            <div>
+                <h3>${product.name}</h3>
+                <h4>Precio: ${product.price}€</h4>
+                <p>Tipo: ${product.sale ? 'venta' : 'compra'}</p>
+                <p>Categoria: ${product.tags[0]}</p>
+            </div>
+        </a>
     ` 
     return newDiv 
 }
