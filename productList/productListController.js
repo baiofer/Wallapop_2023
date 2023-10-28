e59677f1a944709cf2dd5a45e7a976595e0292c5
Fernando Jarilla <fjarilla@gmail.com>
2023-10-28 16:35:00 +0200

    Errors control in productList and empty list of products control

--- productList/productListController.js
+++ productList/productListController.js
@@ -1,14 +1,23 @@
-import { buildProduct } from "./productListView.js"
+import { buildProduct, emptyProducts } from "./productListView.js"
 import { getProducts } from "./productListModel.js"
 import { dispatchEvent } from "../utils/dispatchEvent.js"
 
 
 export const productListController = async (productList) => {
     productList.innerHTML = ''
-    let products
-    products = await getProducts()
-    dispatchEvent('productsLoaded', { type: "success", message: "Se han cargado los productos." }, productList)
-    renderProducts(products, productList)
+    let products = []
+    try {
+        products = await getProducts()
+    } catch (error) {
+        dispatchEvent('productsLoaded', { type: 'error', message: 'Error al cargar los productos.' }, tweetList)
+    }
+    
+    if (products.length === 0) {
+        productList.innerHTML = emptyProducts()
+    } else {
+        dispatchEvent('productsLoaded', { type: "success", message: "Se han cargado los productos." }, productList)
+        renderProducts(products, productList)
+    }
 }
 
 const renderProducts = (products, productList) => {
@@ -17,59 +26,3 @@ const renderProducts = (products, productList) => {
         productList.appendChild(newProduct)
     })
 }
-
-/*
-import { buildTweet, emptyTweets } from './tweetListView.js'
-import { getTweets } from './tweetListModel.js'
-import { dispatchEvent } from "../utils/dispatchEvent.js"
-
-export const tweetListController = async (tweetList) => {
-    tweetList.innerHTML = ''
-    let tweets = []
-    try {
-        dispatchEvent('startLoadingTweets', null, tweetList)
-        tweets = await getTweets()
-    } catch (error) {
-        dispatchEvent('tweetsLoaded', {
-            type: 'error',
-            message: 'Error al cargar los tweets'
-        }, tweetList)
-    } finally {
-        dispatchEvent('finishLoadingTweets', null, tweetList)
-    }
-    
-    // 2.1 Crear estructura de html con los datos del tweet
-    
-    if (tweets.length === 0) {
-        tweetList.innerHTML = emptyTweets()
-    } else {
-        
-        renderTweets(tweets, tweetList)
-        dispatchEvent('tweetsLoaded', {
-            type: 'success',
-            message: 'Tweets cargados correctamente'
-        }, tweetList)
-    }
-    
-}
-
-const renderTweets = (tweets, tweetList) => {
-    tweets.forEach( tweet => {
-        const newTweet = document.createElement('div')
-        newTweet.classList.add('tweet')
-        newTweet.innerHTML = buildTweet(tweet)
-        // 2.2 Añado tweet al DOM
-        tweetList.appendChild(newTweet)
-    })
-}
-
-const createCustomEvent = (type, message) => {
-    const event = new CustomEvent('tweetsLoaded', {
-        detail: {
-            type: type,
-            message: message
-        }
-    })
-    return event
-}
-*/
